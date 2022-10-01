cc16df0f37a75c4b49f808cc2ba075744fbbe9f2
LAPTOP-2O353BVN\gt267 <GT267200@gmail.com>
2022-10-01 16:43:19 +0530

    hello

---
+++ swap.py
@@ -0,0 +1,15 @@
+#take inputs from user for two numbers( x and y)
+
+
+a = input("enter first number : ")
+b = input("enter second number : ") 
+print ("value of a : " , a)
+print ("value of b : " , b)
+temp = a
+print ("value of temp" , temp)
+a = b
+print ("value of x : " , a ) 
+b = temp
+print("value of y : ", b)
+print ("after swap , value of a is " , a)
+print ("after swap , value of b is " , b)
