5ed871843041395e929209153fabe8b8139285a6
LAPTOP-2O353BVN\gt267 <GT267200@gmail.com>
2022-10-31 23:46:31 +0530

    cal1

---
+++ cal1.py
@@ -0,0 +1,5 @@
+# CAL 1
+a = int(input("a: "))
+b = int(input("b: "))
+print(((a + b) ** 2) >= ((a ** 2) + (b ** 2)))
+print(((a - b) ** 2) - 3 * a * b)
--- pro.py
+++ pro.py
@@ -2,3 +2,4 @@ i = 1
 while i <= 4:
     print("*" * i)
     i += 1
+    
