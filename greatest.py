e68bbad900007ea4c9f6057d00c0792e16ffb5bb
LAPTOP-2O353BVN\gt267 <GT267200@gmail.com>
2022-12-02 22:28:08 +0530

    greatest

---
+++ greatest.py
@@ -0,0 +1,12 @@
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+d = int(input("d: "))    
+if a%5 == 0 and a%2==0:
+    print(a)
+if b%5 == 0 and b%2==0:
+    print(b)
+if c%5 == 0 and c%2==0:
+    print(c)
+if d%5 == 0 and d%2==0:
+    print(d)
