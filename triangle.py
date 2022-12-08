fe3a04556b6599cb4ec3db320042046473a63de8
GAURAV <gt267200@gmail.com>
2022-12-08 22:29:57 +0530

    triangle

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a = int(input("s1: "))
+b = int(input("s2: "))
+c = int(input("s3: "))
+d = int(input("s4: "))
+if a == b and b == c and c == d and d == a:
+    print("possible")
+else:
+    print("not possible")
