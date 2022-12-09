91f01fd1389a64deaf1c3a5381bbabd99adc89d1
GAURAV <gt267200@gmail.com>
2022-12-09 19:45:55 +0530

    countdigits

---
+++ countdigits.py
@@ -0,0 +1,13 @@
+a = int(input())
+if a == 1:
+    print("6")
+elif a == 2:
+    print("7")
+elif a == 3:
+    print("8")
+elif a == 4:
+    print("9")
+elif a == 5:
+    print("10")
+else:
+    print("invalid")
