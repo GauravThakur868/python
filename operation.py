d2c8a8f95ab8ab0c23a4cb3a22f111ee7d837fbe
GAURAV <gt267200@gmail.com>
2022-12-12 21:43:09 +0530

    operation

---
+++ operation.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = input()
+if c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "//":
+    print(a//b)
+elif c == "%":
+    print(a%b)
+elif c == "**":
+    print(a**b)
+else:
+    print("invalid")
