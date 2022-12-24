63a43b714be68fc89a34969d16712fcbd94c9e46
GAURAV <gt267200@gmail.com>
2022-12-24 23:29:16 +0530

    reg

--- regex.py
+++ regex.py
@@ -1,1 +1,11 @@
+import re
+value = "this is string"
+output = re.search("^this.*string$",value)
+print(output)
+pattern = r"^(?=.*[a-z])(?=.*[!@#$%^&*()_+=-])(?=.{8,})"
 
+# ^ = starts with it
+# *[] = 
+# ?=  = lookhead assertion
+# {} = from 8 to infinity 
+# r = raw stringazxcxcvbn6+01423
