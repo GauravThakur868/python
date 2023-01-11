612ebac1cc424a585c926a91e6de5baef302e1b9
GAURAV <gt267200@gmail.com>
2023-01-11 23:26:35 +0530

    bitwise

---
+++ bitwis.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+def great(x,y):
+    print(x >= y)
+def small(x,y):
+    print(x <= y)
+def notequal(x,y):
+    print(x != y)
+great(x,y)
+small(x,y)
+notequal(x,y)
