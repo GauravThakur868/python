90a6e57324fcad89341a2702020c197dd962cb1b
GAURAV <gt267200@gmail.com>
2023-01-14 23:12:23 +0530

    max

--- max.py
+++ max.py
@@ -2,4 +2,11 @@ x = int(input())
 y = int(input())
 z = int(input())
 def minofthree(x,y,z):
-    
+    if x <y and x<y:
+        return(x)
+        print(x)
+    elif y< x and y <z:
+        return(y)
+        print(y)
+    elif z<x and z<y:
+        return(z)
