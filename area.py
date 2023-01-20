f2860ef07550e09a2686230c1695d78e2ea6bd85
GAURAV <gt267200@gmail.com>
2023-01-20 23:20:23 +0530

    b

--- area.py
+++ area.py
@@ -3,6 +3,6 @@ h = int(input())
 def area(r,h):
     a = 2*(3.14)*r*15
     b = 2*(3.14)*r*h
-    print()
-    print()
+    print("area1: ","{:.2f}".format(a))
+    print("area2: ","{:.2f}".format(b))
 area(r,h)
