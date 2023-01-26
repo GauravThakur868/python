a0746f26da849ed03e5f31bc4cead69662ef7d3e
GAURAV <gt267200@gmail.com>
2023-01-26 23:10:00 +0530

    eo

--- evenodd.py
+++ evenodd.py
@@ -1,2 +1,5 @@
-list1 = []
+list1 = [22,56,77,99,45,36,87,93,5,78,2,66]
 evenlist1 = list(filter(lambda x : x%2 == 0,list1))
+print("even:",evenlist1)
+oddlist1 = list(filter(lambda x : x%2 != 0,list1))
+print("odd:",oddlist1)
