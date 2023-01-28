0ba46f3f8e40b8f82916e33ea7a9501a5dbfc842
GAURAV <gt267200@gmail.com>
2023-01-28 22:37:41 +0530

    sqr

--- perfectsqr.py
+++ perfectsqr.py
@@ -2,4 +2,7 @@ a = int(input())
 b = int(input())
 def perfectsqr(a,b):
     for i in range(a,b+1):
-        
+        if i**0.5 == int(i**0.5):
+            print(i)
+            break
+perfectsqr(a,b)
