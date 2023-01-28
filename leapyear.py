f54ffcdc8aea6de467108e55eddbf717a46a0996
GAURAV <gt267200@gmail.com>
2023-01-28 22:41:47 +0530

    leapyear

--- leapyear.py
+++ leapyear.py
@@ -2,3 +2,10 @@ year = int(input())
 def isleap(year):
     if year % 100 == 0:
         print(year%100 == 0)
+    elif year%4 == 0:
+        print(year%4 == 0)
+    elif year%400 == 0:
+        print(year%400 == 0)
+    else:
+        print("False")
+isleap(year)
