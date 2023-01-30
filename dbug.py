fb42c0d3de74da487a7622f625e9ed2b1a05d8e9
GAURAV <gt267200@gmail.com>
2023-01-30 22:36:56 +0530

    4

--- dbug.py
+++ dbug.py
@@ -1,4 +1,11 @@
 def function(x,y,z,f):
     ans = (x-y)*z /f
     print("result:"+"{:.2f".format(ans))
-    
+    e = 
+
+    e = 
+x = int(input())
+y = int(input())
+z = int(input())
+f = int(input())
+function(x,y,z,f)
