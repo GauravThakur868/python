4548f22718495168e212cda2424b16bbe5d74c92
GAURAV <gt267200@gmail.com>
2023-01-31 22:25:43 +0530

    remove

--- remove.py
+++ remove.py
@@ -2,4 +2,9 @@ str1 = input()
 ind = int(input())
 def remove(str1,ind):
     if len(str1) <= ind:
-        print()
+        print("index out of range")
+    else:
+        for i in range(0,len(str1)):
+            if i != ind-1:
+                print(str1[i],end="")
+remove(str1,ind)
