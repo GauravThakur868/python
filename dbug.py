90bd7e230527465654a6c32cd63ecf66a3e2a3b2
GAURAV <gt267200@gmail.com>
2023-01-31 22:22:11 +0530

    bug

--- dbug.py
+++ dbug.py
@@ -1,9 +1,10 @@
 def function(x,y,z,f):
     ans = (x-y)*z /f
     print("result:"+"{:.2f".format(ans))
-    e = 
-
-    e = 
+    e = ((x-y)*z)/f
+    print("result:"+"{:.2f}".format(e))
+    e = (x-y)*(z/f)
+    print("result:"+"{:.2f}".format(e))
 x = int(input())
 y = int(input())
 z = int(input())
