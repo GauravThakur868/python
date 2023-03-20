86142abfa0b5231e4a5b01c2a90392b9b0b81b58
GAURAV <gt267200@gmail.com>
2023-03-20 20:54:45 +0530

    3

---
+++ dict21.py
@@ -0,0 +1,11 @@
+k = input('keys: ').split(',')
+print(":values: ")
+d = {}
+l = []
+for i in k:
+    v = list(map(int,input().split(',')))
+    l.append(v)
+    d[i]=v
+print(d)
+for row in zip(*([key]+(value) for key , value in d.items())):
+    print(*row)
