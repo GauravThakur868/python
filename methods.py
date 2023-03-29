95943ea1dacce6966c2f4d4235f12f6cfd0d0053
GAURAV <gt267200@gmail.com>
2023-03-29 23:31:03 +0530

    2

---
+++ methods.py
@@ -0,0 +1,9 @@
+class inputoutstring(object):
+    def getstring(object):
+        a = input()
+        object.a = a
+    def printstring(object):
+        print(object.a.lower())
+strobj = inputoutstring()
+strobj.getstring()
+strobj.printstring()
